7b9216b0c26ded3572554e9d26de50039e767caa
Renato Mateus <renatomateusx@gmail.com>
2021-06-07 18:16:02 -0300

    Initialize Commit

---
+++ MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Renato Mateus on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
